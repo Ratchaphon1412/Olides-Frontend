ef97aba0ae2feef8932fc0e851277d02783c5a8e
kornavit <kornavit.p@ku.th>
2023-06-12 00:37:47 +0700

    navbar edit search by boss

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -23,40 +23,43 @@ onMounted(() => {
                 <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only hidden lg:flex">Your
                     Email</label>
                 <button id="dropdown-button" data-dropdown-toggle="dropdown"
-                    class="flex-shrink-0 z-10 lg:inline-flex hidden items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100"
-                    type="button">All categories <svg aria-hidden="true" class="w-4 h-4 ml-1" fill="currentColor"
-                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    class="flex-shrink-0 z-10 lg:inline-flex hidden items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 border  rounded-l-lg focus:ring-4 focus:outline-none focus:ring-gray-100"
+                    type="button" style="background: rgba(255, 255, 255, 0.32); box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); backdrop-filter: blur(6.1px); -webkit-backdrop-filter: blur(6.1px);">
+                    All categories 
+                    <svg aria-hidden="true" class="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
                             d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                             clip-rule="evenodd"></path>
                     </svg></button>
                 <div id="dropdown"
-                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                    class="z-10 hidden divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+                    style="background: rgba(255, 255, 255, 0.32); box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); backdrop-filter: blur(6.1px); -webkit-backdrop-filter: blur(6.1px);">
                     <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
                         <li>
                             <button type="button"
-                                class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mockups</button>
+                                class="inline-flex w-full px-4 py-2">Mockups</button>
                         </li>
                         <li>
                             <button type="button"
-                                class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Templates</button>
+                                class="inline-flex w-full px-4 py-2">Templates</button>
                         </li>
                         <li>
                             <button type="button"
-                                class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Design</button>
+                                class="inline-flex w-full px-4 py-2">Design</button>
                         </li>
                         <li>
                             <button type="button"
-                                class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Logos</button>
+                                class="inline-flex w-full px-4 py-2">Logos</button>
                         </li>
                     </ul>
                 </div>
                 <div class="relative w-full hidden lg:block">
                     <input type="search" id="search-dropdown"
-                        class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                        class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                        style="background: rgba(255, 255, 255, 0.32); box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); backdrop-filter: blur(6.1px); -webkit-backdrop-filter: blur(6.1px);"
                         placeholder="Search Mockups, Logos, Design Templates..." required>
                     <button type="submit"
-                        class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        class="absolute top-0 right-0 p-2.5 text-sm font-medium bg-[#39CCCC] text-white rounded-r-lg border focus:ring-4 focus:outline-none focus:ring-blue-300">
                         <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -112,39 +115,44 @@ onMounted(() => {
                         Email</label>
                     <button id="dropdown-button" data-dropdown-toggle="dropdown"
                         class="flex-shrink-0 z-10 lg:hidden inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100"
-                        type="button">All categories <svg aria-hidden="true" class="w-4 h-4 ml-1" fill="currentColor"
+                        style="background: rgba(255, 255, 255, 0.32); box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); backdrop-filter: blur(6.1px); -webkit-backdrop-filter: blur(6.1px);"
+                        type="button">
+                        All categories 
+                        <svg aria-hidden="true" class="w-4 h-4 ml-1" fill="currentColor"
                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd"
                                 d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                                 clip-rule="evenodd"></path>
                         </svg></button>
                     <div id="dropdown"
-                        class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
+                        class="z-10 hidden divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+                        style="background: rgba(255, 255, 255, 0.32); box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); backdrop-filter: blur(6.1px); -webkit-backdrop-filter: blur(6.1px);">
+                        <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdown-button">
                             <li>
                                 <button type="button"
-                                    class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mockups</button>
+                                    class="inline-flex w-full px-4 py-2">Mockups</button>
                             </li>
                             <li>
                                 <button type="button"
-                                    class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Templates</button>
+                                    class="inline-flex w-full px-4 py-2">Templates</button>
                             </li>
                             <li>
                                 <button type="button"
-                                    class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Design</button>
+                                    class="inline-flex w-full px-4 py-2">Design</button>
                             </li>
                             <li>
                                 <button type="button"
-                                    class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Logos</button>
+                                    class="inline-flex w-full px-4 py-2">Logos</button>
                             </li>
                         </ul>
                     </div>
                     <div class="relative w-full block lg:hidden">
                         <input type="search" id="search-dropdown"
-                            class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                            class="block p-2.5 w-full z-20 text-sm text-gray-900  rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                            style="background: rgba(255, 255, 255, 0.32); box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); backdrop-filter: blur(6.1px); -webkit-backdrop-filter: blur(6.1px);"
                             placeholder="Search Mockups, Logos, Design Templates..." required>
                         <button type="submit"
-                            class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                            class="absolute top-0 right-0 p-2.5 text-sm font-medium bg-[#39CCCC] text-white  rounded-r-lg border focus:ring-4 focus:outline-none focus:ring-blue-300">
                             <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
