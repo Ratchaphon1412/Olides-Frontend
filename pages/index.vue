dce93b589fd863ceab46ab76721054b49eee9e0c
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-06-23 15:24:43 +0700

    fix component and re structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <Navbar/>
-  <Landing/>
-  <Slide/>
-  <Shortstore/>
+  <Navbar />
+  <SectionsLanding />
+  <Slide />
+  <Shortstore />
 
-  <Platfrom/>
-  <Footer/>
+  <Platfrom />
+  <Footer />
 </template>
 
 <style scoped>
-body{
-  background-image: url('@/assets/css/images/Main_Search.png');
-  background-size:cover;
+body {
+  background-image: url("@/assets/css/images/Main_Search.png");
+  background-size: cover;
   background-repeat: no-repeat;
 }
 </style>
