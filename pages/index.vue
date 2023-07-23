6345d361ed4c30fda119a8e4d3ec382892dc3864
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-07-23 19:42:46 +0700

    rename section SlideCategory

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,9 @@
 <template>
   <NuxtLayout>
     <SectionsLanding />
-    <SectionsSlide />
+    <SectionsSlideCategory />
     <SectionsShortstore />
-  
+
     <SectionsPlatfrom />
     <SectionsReview />
   </NuxtLayout>
