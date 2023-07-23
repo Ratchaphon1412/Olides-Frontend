22fee12a6689acf9b814c25583c18e70d2e9da6c
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-07-23 19:29:38 +0700

    edit responsive and add Slider component

--- components/Sections/Review.vue
+++ components/Sections/Review.vue
@@ -1,9 +1,13 @@
 <template>
-    <main class="py-10 bg-black">
-        <section class="mb-10 text-center font-semibold text-Gray">
-            <p class="text-2xl">Lorem ipsum dolor sit amet.</p>
-            <p >Lorem ipsum dolor sit amet</p>
-        </section>
-        <cardreview/>
-    </main>
+  <main class="py-10 bg-black">
+    <section class="mb-10 text-center font-semibold text-Gray">
+      <p class="text-2xl">Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet</p>
+    </section>
+    <Slider :autoplay="0">
+      <slide v-for="n in 5" :key="n">
+        <CardReview />
+      </slide>
+    </Slider>
+  </main>
 </template>
