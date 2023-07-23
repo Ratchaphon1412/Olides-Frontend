ecffb58fdf28cb3749f43f99f34ff3bda1cd13d6
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-07-23 19:41:16 +0700

    edit reponsive cardImage

--- components/cardimage.vue
+++ components/cardimage.vue
@@ -1,13 +1,13 @@
-<script setup>
-import { ref } from 'vue';
-
-const num = ref(6)
-</script>
+<script setup></script>
 <template>
-    <div class="relative h-[350px] min-w-[191px] lg:min-w-[381px] mr-12" v-for="n in num">
-        <img class="rounded-full mx-auto" src="@/assets/css/images/unsplash_boat.jpg" alt="">
-        <div class="text-center text-[#A0ABC0] mt-4">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima</p>
-        </div>
+  <div class="h-64 max-w-md mr-8 ml-8">
+    <img
+      class="rounded-full mx-auto"
+      src="@/assets/css/images/unsplash_boat.jpg"
+      alt=""
+    />
+    <div class="text-center text-[#A0ABC0] mt-4">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima</p>
     </div>
+  </div>
 </template>
