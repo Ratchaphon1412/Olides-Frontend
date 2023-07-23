8270a564284a28b4e5b0f49efa7b2d15dfb5afe4
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-07-23 19:28:43 +0700

    add Slider auto component to Platfrom

--- components/Sections/Platfrom.vue
+++ components/Sections/Platfrom.vue
@@ -1,73 +1,59 @@
 <script setup>
-import { onMounted } from 'vue'
-import { initFlowbite } from 'flowbite'
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
 
 // initialize components based on data attribute selectors
 onMounted(() => {
-    initFlowbite();
-})
-
-
+  initFlowbite();
+});
+const itemsShow = 1;
+const breakpointsDefault = {
+  480: {
+    itemsToShow: 1,
+    snapAlign: "center",
+  },
+};
+const paragraph = [
+  {
+    title: "PRICING",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, veniam obcaecati, atque, sit explicabo laboriosam doloribus enim iure officia excepturi esse consequuntur est! Tenetur quaerat commodi",
+  },
+  {
+    title: "EASY",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis facere voluptas impedit non enim corporis. Fugiat, non unde pariatur ex itaque aliquid, odit quaerat molestias facilis, facere deserunt nostrum excepturi.",
+  },
+  {
+    title: "SECURITY",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quaerat expedita tempore nesciunt quis eveniet illo vitae at assumenda inventore, est nostrum mollitia saepe. Illo iure itaque numquam id in.",
+  },
+];
 </script>
 
 <template>
-    <main class="bg-black">
-        <p class="text-center text-Light text-4xl font-semibold pt-52 pb-10">Why Choose ODLES</p>
-        <section class="flex md:flex-row flex-col justify-around pb-52">
-            <!-- image IphoneX ODLES -->
-            <img src="@/assets/css/images/iPhoneX.png" class="w-fit self-center" alt="">
-            <div class="self-center w-[80vw] md:w-[50vw]">
-                <div class="carousel w-full">
-                    <div id="slide1" class="carousel-item relative w-full h-96">
-                        <div class="text-Light w-full self-center">
-                            <p class="text-3xl font-semibold text-center">PRICING</p>
-                            <p class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, veniam obcaecati, atque, sit explicabo laboriosam doloribus enim iure officia excepturi esse consequuntur est! Tenetur quaerat commodi</p>
-                        </div>
-                        <div class="absolute flex justify-center transform translate-y-20 left-5 right-5 top-1/2">
-                            <a href="#slide3" class="btn btn-circle btn-ghost">❮</a>
-                            <div class="self-center">
-                                <Icon name="fad:armrecording"/>
-                                <Icon name="fad:mono"/>
-                                <Icon name="fad:mono"/>
-                            </div>
-                            <a href="#slide2" class="btn btn-circle btn-ghost">❯</a>
-                        </div>
-                    </div>
-                    <div id="slide2" class="carousel-item relative w-full h-96">
-                        <div class="text-Light w-full self-center">
-                            <p class="text-3xl font-semibold text-center">PRICING</p>
-                            <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis facere voluptas impedit non enim corporis. Fugiat, non unde pariatur ex itaque aliquid, odit quaerat molestias facilis, facere deserunt nostrum excepturi.</p>
-                        </div>
-                        <div class="absolute flex justify-center transform translate-y-20 left-5 right-5 top-1/2">
-                            <a href="#slide1" class="btn btn-circle btn-ghost">❮</a>
-                            <div class="self-center">
-                                <Icon name="fad:mono"/>
-                                <Icon name="fad:armrecording"/>
-                                <Icon name="fad:mono"/> 
-                            </div>
-                            <a href="#slide3" class="btn btn-circle btn-ghost">❯</a>
-                        </div>
-                    </div>
-                
-                    <div id="slide3" class="carousel-item relative w-full h-96">
-                        <div class="text-Light w-full self-center">
-                            <p class="text-3xl font-semibold text-center">PRICING</p>
-                            <p class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quaerat expedita tempore nesciunt quis eveniet illo vitae at assumenda inventore, est nostrum mollitia saepe. Illo iure itaque numquam id in.</p>
-                        </div>
-                        <div class="absolute flex justify-center transform translate-y-20 left-5 right-5 top-1/2">
-                            <a href="#slide2" class="btn btn-circle btn-ghost">❮</a>
-                            <div class="self-center">
-                                <Icon name="fad:mono"/>
-                                <Icon name="fad:mono"/> 
-                                <Icon name="fad:armrecording"/>
-                            </div>
-                            <a href="#slide1" class="btn btn-circle btn-ghost">❯</a>
-                        </div>
-                    </div>
-
-                </div>
+  <main class="bg-black">
+    <p class="text-center text-Light text-4xl font-semibold p-24">
+      Why Choose ODLES
+    </p>
+    <section class="flex md:flex-row flex-col justify-around">
+      <!-- image IphoneX ODLES -->
+      <img
+        src="@/assets/css/images/iPhoneX.png"
+        class="w-fit self-center"
+        alt=""
+      />
+      <div class="self-center md:w-1/2 w-2/3">
+        <Slider :itemsShow="itemsShow" :breakpointsSetup="breakpointsDefault">
+          <slide v-for="para in paragraph" :key="para">
+            <div class="text-Light w-full m-8">
+              <p class="text-3xl font-semibold text-center">{{ para.title }}</p>
+              <p class="text-left">{{ para.text }}</p>
             </div>
-
+          </slide>
+          <template #control>
+            <pagination />
+          </template>
+        </Slider>
+      </div>
     </section>
-</main>
+  </main>
 </template>
