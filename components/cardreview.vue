1ce821571fe276855e31e689d36476bdf04c1daa
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-07-23 19:41:46 +0700

    edit reponsive cardReview

--- components/cardreview.vue
+++ components/cardreview.vue
@@ -1,20 +1,24 @@
-<script setup>
-import { ref } from 'vue';
-
-const num = ref(6)
-</script>
+<script setup></script>
 <template>
-    <div class="carousel carousel-center w-full p-4 space-x-9 rounded-box">
-        <div class="carousel-item bg-Light flex-col rounded-3xl w-[30vw] p-10" v-for="n in num">
-            <div class="text-center text-Dark mb-5">
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
-                <p>Lorem ipsum dolor sit amet.</p>
-            </div>
-            <p class="w-11/12 text-center mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente aperiam illo similique dolore dolorem facere quos? Aperiam et illo possimus.</p>
-            <div class="flex">
-                <img src="@/assets/css/images/unsplash_boat.jpg" class="rounded-full w-[80px] h-[80px] mr-5" alt="profile">
-                <p class="w-2/4">Lorem ipsum dolor sit amet consectetur</p>
-            </div>
-        </div>
+  <div class="w-full mr-8 ml-8 space-x-9 rounded-box">
+    <div class="bg-Light flex-col rounded-3xl p-10">
+      <div class="text-center text-Dark mb-5">
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <p class="text-center mb-5 w-fit">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
+        aperiam illo similique dolore dolorem facere quos? Aperiam et illo
+        possimus.
+      </p>
+      <div class="flex flex-row items-center">
+        <img
+          src="@/assets/css/images/unsplash_boat.jpg"
+          class="rounded-full w-[80px] h-[80px] mr-5"
+          alt="profile"
+        />
+        <p>Lorem ipsum dolor sit amet consectetur</p>
+      </div>
     </div>
+  </div>
 </template>
